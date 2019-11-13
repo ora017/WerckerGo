f08e6a956b9c6c786de896dfa2539628546797f0
ora017 <57621680+ora017@users.noreply.github.com>
2019-11-13 12:26:32 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -13,15 +13,3 @@ func TestHandleIndexReturnsWithStatusOK(t *testing.T) {
 		t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.Code)
 	}
 }
-
-Commit the file.
-
-6.	Create a file named Dockerfile.
-FROM golang  
-WORKDIR /work
-ADD . .
-RUN go test ./...
-RUN go build -o /bin/myapp .
-WORKDIR /
-RUN rm -r /work
-CMD ["/bin/myapp"]
